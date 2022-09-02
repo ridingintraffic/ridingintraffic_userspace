15a87e3e35e92aca22d187e5dfc369afa6562c57
mike fettis <ridingintraffic@gmail.com>
2022-09-01 22:42:11 -0500

    fix: iterate

--- ridingintraffic.c
+++ ridingintraffic.c
@@ -87,5 +87,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
         break;
     }
-    return process_record_keymap(keycode, record);
+    //return process_record_keymap(keycode, record);
+    return process_record_kb(keycode, record);
 }
