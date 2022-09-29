45a612560033d719888ca6e93722cf78befd5b7f
ridignintraffic <michael.j.fettis@gmail.com>
2022-09-29 11:21:04 -0500

    i think i got the sequencing right now

--- config.h
+++ config.h
@@ -1,1 +1,11 @@
+#pragma once
 
+// Common QMK variables
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAP_CODE_DELAY 10
+
+#include "wrappers.h"
+
+#include "keymaps/lily58.h"
--- ridingintraffic.h
+++ ridingintraffic.h
@@ -5,3 +5,9 @@
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAP_CODE_DELAY 10
+
+#include "wrappers.h"
+
+#ifdef KEYBOARD_lily58
+    #include "keymaps/lily58.h"
+#endif // KEYBOARD_lily58
