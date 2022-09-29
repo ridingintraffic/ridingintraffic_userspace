6d5bd37abfbaab065932bd167d1565500dcf50a8
ridignintraffic <michael.j.fettis@gmail.com>
2022-09-29 10:32:34 -0500

    feat: starting to break some things up

---
+++ keymaps/lily58.h
@@ -0,0 +1,6 @@
+#define LILY_BASE \
+KC_GESC,   ________________NUMBER_LEFT________________,                  ________________NUMBER_RIGHT_______________,    KC_GRV, \
+KC_GESC,   _________________QWERTY_L1_________________,                  _________________QWERTY_R1_________________,    KC_MINS, \
+KC_TAB,    _________________QWERTY_L2_________________,                  _________________QWERTY_R2_________________, KC_QUOT, \
+KC_LSFT,   _________________QWERTY_L3_________________, KC_LBRC,  KC_RBRC,  _________________QWERTY_R3_________________,  KC_RSFT, \
+                                 LT(2, KC_LALT), __THUMBS_3L__, __THUMBS_3R__, LT(1, KC_LCTL)
---
+++ ridingintraffic.h
@@ -0,0 +1,5 @@
+#include "wrappers.h"
+
+#ifdef KEYBOARD_lily58
+    #include "keymaps/lily58.h"
+#endif
