11f0bf0364e07d0ea9928aa3849767aa5249b6ed
mike fettis <michael.j.fettis@gmail.com>
2022-09-29 23:14:21 -0500

    feat: fingerpunch builds

---
+++ keymaps/ffkb.h
@@ -0,0 +1,7 @@
+#define LAYOUT_wrapper_ffkb(...) LAYOUT_ffkb(__VA_ARGS__)
+
+#define BASE \
+KC_GESC,   _________________QWERTY_L1_________________,                  _________________QWERTY_R1_________________,    KC_MINS, \
+KC_TAB,    _________________QWERTY_L2_________________,                  _________________QWERTY_R2_________________, KC_QUOT, \
+KC_LSFT,   _________________QWERTY_L3_________________, KC_SPC, _________________QWERTY_R3_________________,  KC_RSFT, \
+                                            KC_SPC,      __THUMBS_3L__, __THUMBS_3R__, KC_SPC
