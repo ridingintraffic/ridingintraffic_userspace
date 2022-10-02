41660b77893c59d7070ad12403fff86e620014ce
mike fettis <michael.j.fettis@gmail.com>
2022-10-02 11:49:12 -0500

    feat: adding plank and plover and lili keymaps, adding kb2040 to builds

--- ridingintraffic.c
+++ ridingintraffic.c
@@ -1,11 +1,6 @@
 #include "quantum.h"
 #include QMK_KEYBOARD_H
 
-#define KC_CPYP LGUI(LSFT(LCTL(KC_4)))  // Copy portion of screen  to clipboard
-#define KC_CPYF LGUI(LSFT(LCTL(KC_3)))  // Copy full screen to clipboard
-#define KC_CPYPSS LGUI(LSFT(KC_4))  // Copy portion of screen  to desktop
-#define KC_CPYFSS LGUI(LSFT(KC_3))  // Copy full screen to desktop
-
 #define KC_ZOOMMUTE LGUI(LSFT(KC_A))  // zoom mute / unmute
 #define KC_ZOOMSCREENSHARE LGUI(LSFT(KC_S))  // zoom screenshare start stop
 #define KC_ZOOMSCREENSHAREPAUSE LGUI(LSFT(KC_T))  // zoom screenshare pause resume
