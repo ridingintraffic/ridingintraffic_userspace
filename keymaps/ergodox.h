85ea68d7b6e7bff0ebcc00522124e63fcf7df9da
mike fettis <michael.j.fettis@gmail.com>
2022-10-02 22:14:35 -0500

    feat: adding ergodox

--- keymaps/ergodox.h
+++ keymaps/ergodox.h
@@ -1,11 +1,11 @@
 #define LAYOUT_wrapper_ergodox_pretty(...) LAYOUT_ergodox_pretty(__VA_ARGS__)
 
 #define BASE \
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS, \
-  KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_SPC,             KC_SPC,     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS, \
-  KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SPC, GUI_T(KC_QUOT), \
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),                  MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT, \
-  KC_SPC, KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, KC_SPC, \
-                                                           ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC), \
-                                                                          KC_HOME,                 KC_PGUP, \
-                                                         KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_TAB, KC_ENT 
+  KC_EQL,          ________________NUMBER_LEFT________________, KC_LEFT,             KC_RGHT, ________________NUMBER_RIGHT_______________, KC_EQL, \
+  KC_DEL,          _________________QWERTY_L1_________________, KC_SPC,              KC_SPC,  _________________QWERTY_R1_________________, KC_MINS, \
+  KC_BSPC,         _________________QWERTY_L2_________________,                               _________________QWERTY_R2_________________, KC_QUOT, \
+  KC_LSFT,         _________________QWERTY_L3_________________,                               _________________QWERTY_R3_________________, KC_RSFT, \
+  KC_SPC, KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                         KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, KC_SPC, \
+                                                ALT_T(KC_APP), KC_LGUI,             KC_LALT, CTL_T(KC_ESC), \
+                                                               KC_HOME,             KC_PGUP, \
+                                                KC_SPC, KC_BSPC, KC_END,            KC_PGDN, KC_TAB, KC_ENT 
--- keymaps/lily58.h
+++ keymaps/lily58.h
@@ -1,10 +1,10 @@
 #define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 
 #define BASE \
-KC_GESC,   ________________NUMBER_LEFT________________,                  ________________NUMBER_RIGHT_______________,    KC_GRV, \
-KC_GESC,   _________________QWERTY_L1_________________,                  _________________QWERTY_R1_________________,    KC_MINS, \
+KC_GESC,   ________________NUMBER_LEFT________________,                  ________________NUMBER_RIGHT_______________, KC_EQL, \
+KC_GESC,   _________________QWERTY_L1_________________,                  _________________QWERTY_R1_________________, KC_MINS, \
 KC_TAB,    _________________QWERTY_L2_________________,                  _________________QWERTY_R2_________________, KC_QUOT, \
-KC_LSFT,   _________________QWERTY_L3_________________, KC_LBRC,  KC_RBRC,  _________________QWERTY_R3_________________,  KC_RSFT, \
+KC_LSFT,   _________________QWERTY_L3_________________, KC_LBRC,  KC_RBRC,  _________________QWERTY_R3_________________,  KC_BSLS, \
                                  LT(2, KC_LALT), __THUMBS_3L__, __THUMBS_3R__, LT(1, KC_LCTL)
 
 #define RAISE \
