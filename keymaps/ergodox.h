8f9fcd6bd18a8009b042ee0bf447ef10792b6bc9
mike fettis <ridingintraffic@users.noreply.github.com>
2022-10-06 12:06:10 -0500

    fix: planck and ergodox are working now (#9)

--- keymaps/ergodox.h
+++ keymaps/ergodox.h
@@ -1,11 +1,11 @@
 #define LAYOUT_wrapper_ergodox_pretty(...) LAYOUT_ergodox_pretty(__VA_ARGS__)
 
 #define BASE \
-  KC_EQL,          ________________NUMBER_LEFT________________, KC_LEFT,             KC_RGHT, ________________NUMBER_RIGHT_______________, KC_EQL, \
-  KC_DEL,          _________________QWERTY_L1_________________, KC_SPC,              KC_SPC,  _________________QWERTY_R1_________________, KC_MINS, \
-  KC_BSPC,         _________________QWERTY_L2_________________,                               _________________QWERTY_R2_________________, KC_QUOT, \
+  KC_EQL,          ________________NUMBER_LEFT________________, KC_NO,             KC_NO, ________________NUMBER_RIGHT_______________, KC_EQL, \
+  KC_DEL,          _________________QWERTY_L1_________________, KC_NO,              KC_NO,  _________________QWERTY_R1_________________, KC_MINS, \
+  KC_BSPC,         _________________QWERTY_L2_________________, KC_NO,               KC_NO,   _________________QWERTY_R2_________________, KC_QUOT, \
   KC_LSFT,         _________________QWERTY_L3_________________,                               _________________QWERTY_R3_________________, KC_RSFT, \
   KC_SPC, KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                         KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, KC_SPC, \
                                                 ALT_T(KC_APP), KC_LGUI,             KC_LALT, CTL_T(KC_ESC), \
                                                                KC_HOME,             KC_PGUP, \
-                                                KC_SPC, KC_BSPC, KC_END,            KC_PGDN, KC_TAB, KC_ENT 
+                                                KC_SPC, KC_BSPC, KC_END,            KC_PGDN, KC_TAB, KC_ENT
--- keymaps/planck.h
+++ keymaps/planck.h
@@ -4,16 +4,16 @@
 KC_GESC,   _________________QWERTY_L1_________________,  _________________QWERTY_R1_________________,    KC_MINS, \
 KC_TAB,    _________________QWERTY_L2_________________,  _________________QWERTY_R2_________________, KC_QUOT, \
 KC_LSFT,   _________________QWERTY_L3_________________,  _________________QWERTY_R3_________________,  KC_RSFT, \
-KC_SPC, LT(1, KC_LCTL), LT(2, KC_LALT), __THUMBS_3L__, __THUMBS_3R__, LT(1, KC_LCTL), LT(2, KC_LALT),KC_SPC
+KC_NO,   LT(1, KC_LCTL), LT(2, KC_LALT), __THUMBS_3L__, __THUMBS_3R__, LT(1, KC_LCTL), LT(2, KC_LALT), KC_SPC
 
 #define RAISE \
 KC_GESC,   _________________QWERTY_L1_________________,  _________________QWERTY_R1_________________,    KC_MINS, \
 KC_TAB,    _________________QWERTY_L2_________________,  _________________QWERTY_R2_________________, KC_QUOT, \
 KC_LSFT,   _________________QWERTY_L3_________________,  _________________QWERTY_R3_________________,  KC_RSFT, \
-KC_SPC, LT(1, KC_LCTL), LT(2, KC_LALT), __THUMBS_3L__, __THUMBS_3R__, LT(1, KC_LCTL), LT(2, KC_LALT),KC_SPC
+KC_NO,  LT(1, KC_LCTL), LT(2, KC_LALT), __THUMBS_3L__, __THUMBS_3R__, LT(1, KC_LCTL), LT(2, KC_LALT), KC_SPC
 
 #define LOWER \
 KC_GESC,   _________________QWERTY_L1_________________,  _________________QWERTY_R1_________________,    KC_MINS, \
 KC_TAB,    _________________QWERTY_L2_________________,  _________________QWERTY_R2_________________, KC_QUOT, \
 KC_LSFT,   _________________QWERTY_L3_________________,  _________________QWERTY_R3_________________,  KC_RSFT, \
-KC_SPC, LT(1, KC_LCTL), LT(2, KC_LALT), __THUMBS_3L__, __THUMBS_3R__, LT(1, KC_LCTL), LT(2, KC_LALT),KC_SPC
+KC_NO,  LT(1, KC_LCTL), LT(2, KC_LALT), __THUMBS_3L__, __THUMBS_3R__, LT(1, KC_LCTL), LT(2, KC_LALT), KC_SPC
