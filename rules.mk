3ec66a9599cb34563314e2216464cff64981d61e
mike fettis <ridingintraffic@users.noreply.github.com>
2022-10-27 18:13:58 -0500

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -42,5 +42,5 @@ ifeq ($(strip $(KEYBOARD)), planck/rev6)
 endif
 # planck
 ifeq ($(strip $(KEYBOARD)), fingerpunch/sweeeeep)
-        PIN_COMPATIBLE=elite_c
+        PIN_COMPATIBLE = elite_c
 endif
