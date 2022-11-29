53a5414ee57d2d479276545bafa62bbd13c21c37
ridignintraffic <michael.j.fettis@gmail.com>
2022-11-29 12:39:23 -0600

    fix: at least corne should work

--- keymaps/ffkb.h
+++ keymaps/ffkb.h
@@ -8,7 +8,7 @@ KC_LSFT,   _________________QWERTY_L3_________________,                  _______
                                                               KC_SPC
 
 #define RAISE \
-KC_GESC,   _________________RAISE_L1__________________,                  _________________RAISE_R1__________________, KC_MINS, \
+KC_GESC,   _________________RAISE_L1__________________,                  _________________RAISE_R1__________________, KC_MINUS, \
 KC_TAB,    _________________RAISE_L2__________________,                  _________________RAISE_R2__________________, KC_QUOT, \
 KC_LSFT,   _________________RAISE_L3__________________,                  _________________RAISE_R3__________________,  KC_BSLS, \
                       KC_SPC,      __THUMBS_3L_RAISE__,                __THUMBS_3R_RAISE__, KC_SPC, \
@@ -20,8 +20,8 @@ KC_TAB,    _________________LOWER_L2__________________,                  _______
 KC_LSFT,   _________________LOWER_L3__________________,                  _________________LOWER_R3__________________,  KC_BSLS, \
                         KC_SPC,      __THUMBS_3L_LOWER__,                __THUMBS_3R_LOWER__, KC_SPC, \
                                                              KC_SPC
-#define ACTION\
-KC_TILD,   _________________ACTION_L1_________________,              _________________ACTION_R1_________________,    KC_MINS, \
+#define NOT_ACTION\
+KC_TILD,   _________________ACTION_L1_________________,              _________________ACTION_R1_________________,    KC_MINUS, \
 KC_TAB,    _________________ACTION_L2_________________,              _________________ACTION_R2_________________, KC_QUOT, \
 KC_LSFT,   _________________ACTION_L3_________________,              _________________ACTION_R3_________________,  KC_BSLS, \
                               KC_SPC,  __THUMBS_3L_LOWER__, __THUMBS_3R_LOWER__, KC_SPC,KC_SPC
