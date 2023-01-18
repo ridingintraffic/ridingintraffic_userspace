e31c9c89b084b4ff682f2c65448fb173d15eb3e7
mike fettis <michael.j.fettis@gmail.com>
2023-01-18 17:04:18 -0600

    feat: adding in sweet16

--- keymaps/sweet16.h
+++ keymaps/sweet16.h
@@ -3,7 +3,7 @@
 #define BASE \
 KC_7, KC_8,   KC_9,   LT(1, KC_VOLU), \
 KC_4, KC_5,   KC_6,   LT(1, KC_VOLD), \
-KC_1, KC_2,   KC_3,   KC_ZOOMMUTE, \
+KC_1, KC_2,   KC_3,   LGUI(LSFT(KC_A)), \
 KC_0, KC_ENT, LT(1, LCTL(KC_LEFT)), LT(1, LCTL(KC_RIGHT))
 
 #define RAISE \
