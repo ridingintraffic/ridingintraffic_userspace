81c76ee3b3c16dc4e65dad35eb59f5775ed8ab81
mike fettis <ridingintraffic@users.noreply.github.com>
2023-04-11 11:31:06 -0500

    Update sweet16.h

--- keymaps/sweet16.h
+++ keymaps/sweet16.h
@@ -2,7 +2,7 @@
 
 #define BASE \
 KC_7, KC_8,   KC_9,   KC_VOLU, \
-KC_4, KC_5,   KC_6,   KC_VOLU, \
+KC_4, KC_5,   KC_6,   KC_VOLD, \
 KC_1, KC_2,   KC_3,   KC_MPLY, \
 KC_0, KC_ENT, LT(1, LCTL(KC_LEFT)), LGUI(LSFT(KC_A))
 
