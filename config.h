aab6a6536c32ed0f88d40d2c03e6228cd1fe3e4b
mike fettis <ridingintraffic@users.noreply.github.com>
2023-05-04 16:07:29 -0500

    turning off permissive hold

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 #pragma once
 
 // Common QMK variables
-#define TAPPING_TERM 150
-#define PERMISSIVE_HOLD
+#define TAPPING_TERM 100
+//#define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-#define  QUICK_TAP_TERM 150
+#define  QUICK_TAP_TERM 100
 
 #ifdef KEYBOARD_lily58
     #include "keymaps/lily58.h"
